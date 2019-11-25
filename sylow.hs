6df6c3d618f3e7e381eb138924c7e9f50ffa2322
E. G. Bland <egb528@york.ac.uk>
2019-11-25 00:25:34 +0000

    add sylowPs function and comments

--- sylow.hs
+++ sylow.hs
@@ -1,3 +1,6 @@
+import Data.List -- for nub
+
+-- gets the list of prime factors of n.
 primeFact :: Int -> [Int]
 primeFact 1 = []
 primeFact n
@@ -6,9 +9,18 @@ primeFact n
     where factors = take 1 $ filter (\x -> (mod n x) == 0) [2..n-1]
 
 -- taken from Higemaru on stackoverflow, https://stackoverflow.com/questions/19554984/haskell-count-occurrences-function
+-- gets the number of instances of x in the second argument list.
 instanceCt :: Eq a => a -> [a] -> Int
 instanceCt x = length.filter (x==)
 
+-- gets the different valid p's for the Sylow p-subgroups of a group of order n.
+sylowPs :: Int -> [Int]
+sylowPs n
+    | n < 2 = []
+    | otherwise = nub $ primeFact n
+
+-- gets the possible numbers of Sylow p-subgroups of a group of order n.
+-- used to answer a question about whether or not simple groups of order n are possible - if only one Sylow p-subgroup exists for some p, then there exists a normal subgroup with order p^a, where a is the power of p in the prime decomposition of n.
 sylowSubgpCt :: Int -> Int -> [Int]
 sylowSubgpCt p n
     | n < 2 = error "n should be at least 2."
